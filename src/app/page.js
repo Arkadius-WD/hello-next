3455aaeeb2c869bf1925a99332fe8748022de23d
Arkadius-WD <arkadiusz.kamyczek@gmail.com>
2024-04-12 11:59:42 +0200

    create header and footer

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,12 @@
+import { Footer } from "./components/Footer";
+import { Header } from "./components/Header";
+
 export default function Home() {
 	return (
 		<main>
+			<Header />
 			<h1>Hello next!</h1>
+			<Footer />
 		</main>
 	);
 }
